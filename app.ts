e4c0eee3c71f5d5cc3ed0b4ce4be118548eb964f
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 17:33:25 +0900

    Working with Enums

--- app.ts
+++ app.ts
@@ -5,16 +5,33 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Jonghyeon',
+//   age: 28,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
   name: 'Jonghyeon',
   age: 28,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
 
 // person.role.push('admin');
@@ -30,4 +47,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   //   console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is admin');
 }
