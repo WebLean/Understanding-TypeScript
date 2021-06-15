17a01d7fe805ba14112817774a4292939d7e1f6e
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 17:10:44 +0900

    Arrays Types

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 // hover it over
 // We should more specific
 
-const person: {
-  name: string;
-  age: number;
-} = {
-  // const person = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: 'Jonghyeon',
   age: 28,
+  hobbies: ['Sports', 'Cooking'],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
