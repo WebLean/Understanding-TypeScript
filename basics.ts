9f7ea4b599f2ab3e2d43d9507f5b0346944cbafb
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 17:54:14 +0900

    Literal Types

--- basics.ts
+++ basics.ts
@@ -14,8 +14,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 let number1 = 5;
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: ';
-resultPhrase = 0;
+let resultPhrase = 'Result is: ';
 
 const result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
