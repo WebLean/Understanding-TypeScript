d07beb1947c6cabc85e6761bddc81d94c630977a
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 17:20:09 +0900

    Working with Tuples

--- app.ts
+++ app.ts
@@ -5,11 +5,22 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Jonghyeon',
   age: 28,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -18,4 +29,5 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+  //   console.log(hobby.map()); // !!! ERROR !!!
 }
