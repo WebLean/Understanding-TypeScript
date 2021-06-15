620723c82d4a712670cdb10f3b8c73c40c67d278
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 17:43:59 +0900

    Union Types

--- app.ts
+++ app.ts
@@ -1,54 +1,15 @@
-// hover it over
-// We should more specific
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
 
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: 'Jonghyeon',
-//   age: 28,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author'],
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = 'ADMIN',
-  READ_ONLY = 100,
-  AUTHOR = 200,
+  return result;
 }
 
-const person = {
-  name: 'Jonghyeon',
-  age: 28,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
-};
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user']
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  //   console.log(hobby.map()); // !!! ERROR !!!
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log('is admin');
-}
+const combinedNames = combine('Max', 'Anna');
