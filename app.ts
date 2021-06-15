38ae115ae8f04b6a05eecee8a9424447fc179019
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 10:42:52 +0900

    Object Types

--- app.ts
+++ app.ts
@@ -1,21 +1,13 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  //   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //     throw new Error('Incorrect input');
-  //   }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+// hover it over
+// We should more specific
 
-// TODO: Hover on variable name, different result between let and const
-let number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-resultPhrase = 0;
+const person: {
+  name: string;
+  age: number;
+} = {
+  // const person = {
+  name: 'Jonghyeon',
+  age: 28,
+};
 
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+console.log(person.name);
