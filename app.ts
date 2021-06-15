571cf81882df2f11fb337e9f9a7ee5703a60238e
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-15 18:58:31 +0900

    Functions as Types

--- app.ts
+++ app.ts
@@ -13,4 +13,12 @@ function printResult2(num: number): undefined {
 
 printResult(add(5, 12));
 
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
 // let someValue: undefined;
