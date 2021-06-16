d05caa9bd4cc12adcd0db95e0a2f85d5561481d9
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-16 09:20:07 +0900

    The "never" Type

--- app.ts
+++ app.ts
@@ -6,3 +6,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
+
+generateError('An error occurred', 500);
