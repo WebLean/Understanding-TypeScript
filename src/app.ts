5c9ec29d71544ab315c0cba4403346317036439b
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-16 13:33:53 +0900

    Code Quality Options

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
+let appId = 'abc';
 const button = document.querySelector('button');
 
-function clickHandler(message: string) {
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  // return
+}
+
+function clickHandler(message: string, age: number) {
+  let username;
   console.log('Clicked! ' + message);
 }
 
 // a comment
 if (button) {
-  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+  button.addEventListener(
+    'click',
+    clickHandler.bind(null, "You're welcome!", 30)
+  );
 }
