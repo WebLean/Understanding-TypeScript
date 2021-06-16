bcdc06f5fc983868601ebb139e5743c91f589049
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-16 17:50:45 +0900

    Arrow Functions

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,30 @@ let age = 30;
 
 age = 29;
 
-// "var" has Global scope
-var result;
+var result; // "var" has Global scope
 
-function add(a: number, b: number) {
-  // "let" has Block scope
-  let result2;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result2; // "let" has Block scope
+//   result = a + b;
+//   return result;
+// }
 
 console.log(result);
 
-if (age > 20) {
-  var isOld = true;
-  let isOld2 = true;
-}
+// if (age > 20) {
+//   var isOld = true;
+//   let isOld2 = true;
+// }
 
-// isOld make Error in TypeScript, but in JavaScript, It's not
-console.log(isOld);
+// // isOld make Error in TypeScript, but in JavaScript, It's not
+// console.log(isOld);
+
+const add = (a: number, b: number) => a + b;
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+if (button) button.addEventListener('click', event => console.log(event));
+
+printOutput(add(5, 2));
