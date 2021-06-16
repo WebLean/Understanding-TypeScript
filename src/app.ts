c6feb1710fcfcc4d165142861d124b4b877dee3f
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-16 22:21:52 +0900

    Array & Object Destructuring

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,8 @@
-const userName = 'Max';
-// username = "Maximilian";
-let age = 30;
-
-age = 29;
-
-var result; // "var" has Global scope
-
-// function add(a: number, b: number) {
-//   let result2; // "let" has Block scope
-//   result = a + b;
-//   return result;
-// }
-
-console.log(result);
-
-// if (age > 20) {
-//   var isOld = true;
-//   let isOld2 = true;
-// }
-
-// // isOld make Error in TypeScript, but in JavaScript, It's not
-// console.log(isOld);
-
-// const add = (a: number, b: number = 1) => a + b;
-// console.log(add(2, 5));
-
-// const printOutput: (a: number | string) => void = output => console.log(output);
-
-// const button = document.querySelector('button');
-// if (button) button.addEventListener('click', event => console.log(event));
-
-// printOutput(add(5));
-
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
 activeHobbies.push(...hobbies);
 
-const person = { name: 'Max', age: 30 };
+const person = { firstName: 'Max', age: 30 };
 const copiedPerson = { ...person };
 
 const add = (...numbers: number[]) => {
@@ -45,3 +11,10 @@ const add = (...numbers: number[]) => {
 
 const addedNumbers = add(5, 10, 2, 3.7);
 console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age);
