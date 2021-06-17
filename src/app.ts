ccda5c6a1290549f09b8301ebc13481806bcb220
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-17 18:47:11 +0900

    Optional Parameters & Properties

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,8 @@ add = (n1: number, n2: number) => {
 };
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -18,19 +19,25 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
-let user1: Greetable = new Person('Jonghyeon');
+let user1: Greetable = new Person();
 // user1.name = 'hi';
 
 user1.greet('Hi there - I am');
