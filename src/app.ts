faa738f129f2d4482b6ebf79fa3d3e6f36575c9c
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-18 13:26:55 +0900

    InterSection Types

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,23 @@
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-  (a: number, b: number): number;
-}
-
-let add: AddFn;
-
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+type Admin = {
+  name: string;
+  privileges: string[];
 };
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
-}
+type Employee = {
+  name: string;
+  startDate: Date;
+};
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
+// interface ElevatedEmployee extends Employee, Admin {}
+type ElevatedEmployee = Admin & Employee;
 
-class Person implements Greetable {
-  name?: string;
-  age = 30;
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
 
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
-  }
+type Combinable = string | number;
+type Numeric = number | boolean;
 
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name);
-    } else {
-      console.log('Hi!');
-    }
-  }
-}
-
-let user1: Greetable = new Person();
-// user1.name = 'hi';
-
-user1.greet('Hi there - I am');
-console.log(user1);
+type Universal = Combinable & Numeric;
