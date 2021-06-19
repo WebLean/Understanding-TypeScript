84c5d955761855ec61f5108a52cf3a1111af10b2
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-19 23:43:17 +0900

    Working with Constraints

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,9 @@
 //     // data.split(' ');
 // })
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj);
