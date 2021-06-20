71f69cb65967a7e3943d5135964a0575c7d7197c
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-20 16:55:16 +0900

    A First Class Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,17 @@
-// const names: Array<string> = []; // string[]
-// // names[0].split(' ');
-
-// const promise : Promise<number> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(10);
-//   }, 2000);
-// });
-
-// promise.then(data => {
-//     // data.split(' ');
-// })
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function Logger(constructor: Function) {
+  console.log('Logging...');
+  console.log(constructor);
 }
 
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj);
+@Logger
+class Person {
+  name = 'Max';
 
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = 'Got no value';
-  if (element.length === 1) {
-    descriptionText = 'Got 1 elements.';
-  } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.';
-  }
-  return [element, descriptionText];
-}
-
-console.log(countAndDescribe(['Sports', 'Cooking']));
-
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return 'Value: ' + obj[key];
-}
-
-console.log(extractAndConvert({ name: 'Jonghyeon' }, 'name'));
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    if (this.data.indexOf(item) === -1) {
-      return;
-    }
-    this.data.splice(this.data.indexOf(item), 1); // Can be -1
-  }
-
-  getItems() {
-    return [...this.data];
+  constructor() {
+    console.log('Creating person object...');
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Jonghyeon');
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-console.log(textStorage.getItems());
+const pers = new Person();
 
-const numberStorage = new DataStorage<number>();
-numberStorage.addItem(1);
-numberStorage.addItem(2);
-numberStorage.addItem(3);
-console.log(numberStorage.getItems());
-
-// const objStorage = new DataStorage<object>();
-// const jonghyeonObj = { name: 'Jonghyeon' };
-// objStorage.addItem(jonghyeonObj);
-// objStorage.addItem({ name: 'Max' });
-// // ...
-// objStorage.removeItem(jonghyeonObj); // Error solution cause of reference type
-// console.log(objStorage.getItems());
-
-interface CourseGoal {
-  title: string;
-  description: string;
-  completeUntil: Date;
-}
-
-function createCourseGoal(title: string, description: string, date: Date) {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ['Jonghyeon', 'Max'];
-// names.push('Manu');
-// names.pop();
+console.log(pers);
