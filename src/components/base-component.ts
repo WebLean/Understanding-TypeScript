c878f4881d1cd8c0371584b82a24da490abcda26
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-22 12:00:58 +0900

    Organizing Files & Folders

---
+++ src/components/base-component.ts
@@ -0,0 +1,44 @@
+namespace App {
+  // Component Base Class
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementID: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementID)! as T;
+
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBegging: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBegging ? 'afterbegin' : 'beforeend',
+        this.element
+      );
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
---
+++ src/util/validation.ts
@@ -0,0 +1,26 @@
+namespace App {
+  // Validation
+  export interface Validatable {
+    value: string;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+  export function validate(validatableInput: Validatable) {
+    let isValid = true;
+    const { value, required, minLength, maxLength, min, max } =
+      validatableInput;
+
+    if (required) isValid = isValid && value.trim().length > 0;
+    if (minLength != null && typeof value === 'string')
+      isValid = isValid && value.length >= minLength;
+    if (maxLength != null && typeof value === 'string')
+      isValid = isValid && value.length <= maxLength;
+    if (min != null) isValid = isValid && +value >= min;
+    if (max != null) isValid = isValid && +value <= max;
+
+    return isValid;
+  }
+}
