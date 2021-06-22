340d03437ad52fbf412c735e476e70a12de4fb31
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-22 19:03:18 +0900

    TypeScript-embracing: class-validator

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
@@ -13,6 +14,16 @@ const products = [
   { title: 'A Carpet', price: 29.99 },
   { title: 'A Book', price: 10.99 },
 ];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERORRS!');
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product('A Book', 12.99);
 
