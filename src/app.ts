1add9a830a90a8decb3cb8ebd3e6d58efb3724e6
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-22 18:38:38 +0900

    Using "declare" as a "Last Resort" : Last choice after all other trying

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import { ProjectList } from './components/project-list';
 
 import _ from 'lodash';
 
+declare var GLOBAL: string;
+
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
 
 console.log(_.shuffle([1, 2, 3]));
+
+console.log(GLOBAL);
