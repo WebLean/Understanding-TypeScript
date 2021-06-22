d7df80bd618647354f31ae153b2603f87363ae81
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-22 18:34:05 +0900

    Using JavaScript (!) Libraries with TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
 
+import _ from 'lodash';
+
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
 
-console.log('Hi!');
+console.log(_.shuffle([1, 2, 3]));
